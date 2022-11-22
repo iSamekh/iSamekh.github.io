0b4bb695c8b9b34e4f1609b47dff30899163bcec
Eric Gomes <31543471+iSamekh@users.noreply.github.com>
2022-11-22 13:54:02 -0300

    Counter

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let n = 0;
+
 function test () {
+  n++;
   document.querySelector('html').style.backgroundColor = `rgb(${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)})`; 
+  document.querySelector('p').value = n + " cliques";
 }
