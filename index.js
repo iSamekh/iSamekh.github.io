1c11dcb1b2a4db40c00ffb7e7a8fc7a2bc2e46b2
Eric Gomes <31543471+iSamekh@users.noreply.github.com>
2022-11-22 13:57:03 -0300

    42 try

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let n = 0;
 function test () {
   n++;
   document.querySelector('html').style.backgroundColor = `rgb(${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)})`; 
-  document.querySelector('p').value = n + " cliques";
+  document.querySelector('p').innerHTML = n + " cliques";
 }
