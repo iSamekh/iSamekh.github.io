1a1505d56670b809617629780908d4134f955e6f
Eric Gomes <31543471+iSamekh@users.noreply.github.com>
2022-11-29 10:28:22 -0300

    Page Content test

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function toAboutMe() {
-  document.querySelector("#aboutMe").style.opacity = "1";
-  document.querySelector("#knowledgePage").style.opacity = "0";
-  document.querySelector("#projectsPage").style.opacity = "0";
-  document.querySelector("#contactPage").style.opacity = "0";
+  document.querySelectorAll(".pageContent")[0].style.opacity = "1";
+  document.querySelectorAll(".pageContent")[1].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[2].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[3].style.opacity = "0";
 }
 
 function toKnowledgePage() {
-  document.querySelector("#aboutMe").style.opacity = "0";
-  document.querySelector("#knowledgePage").style.opacity = "1";
-  document.querySelector("#projectsPage").style.opacity = "0";
-  document.querySelector("#contactPage").style.opacity = "0";
+  document.querySelectorAll(".pageContent")[0].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[1].style.opacity = "1";
+  document.querySelectorAll(".pageContent")[2].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[3].style.opacity = "0";
 }
 
 function toProjectsPage() {
-  document.querySelector("#aboutMe").style.opacity = "0";
-  document.querySelector("#knowledgePage").style.opacity = "0";
-  document.querySelector("#projectsPage").style.opacity = "1";
-  document.querySelector("#contactPage").style.opacity = "0";
+  document.querySelectorAll(".pageContent")[0].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[1].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[2].style.opacity = "1";
+  document.querySelectorAll(".pageContent")[3].style.opacity = "0";
 }
 
 function toContactPage() {
-  document.querySelector("#aboutMe").style.opacity = "0";
-  document.querySelector("#knowledgePage").style.opacity = "0";
-  document.querySelector("#projectsPage").style.opacity = "0";
-  document.querySelector("#contactPage").style.opacity = "1";
+  document.querySelectorAll(".pageContent")[0].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[1].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[2].style.opacity = "0";
+  document.querySelectorAll(".pageContent")[3].style.opacity = "1";
 }
 
 //https://stackoverflow.com/questions/58736328/horizontally-scrolling-by-a-full-page-on-key-press
