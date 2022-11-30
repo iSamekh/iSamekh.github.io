df2329b006423e4afb0ae3af2978521bc34d858e
Eric Gomes <31543471+iSamekh@users.noreply.github.com>
2022-11-30 10:18:37 -0300

    Config Slide

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
+const pageContent = document.querySelectorAll(".pageContent");
+const config = document.querySelector("#config");
+
 function toAboutMe() {
-  document.querySelectorAll(".pageContent")[0].style.opacity = "1";
-  document.querySelectorAll(".pageContent")[1].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[2].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[3].style.opacity = "0";
+  pageContent[0].style.opacity = "1";
+  pageContent[1].style.opacity = "0";
+  pageContent[2].style.opacity = "0";
+  pageContent[3].style.opacity = "0";
+  config.style.top = "0vh";
+  config.style.right = "5vw";
+  config.style.bottom = "5vh";
+  config.style.left = "0vw";
 }
 
 function toKnowledgePage() {
-  document.querySelectorAll(".pageContent")[0].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[1].style.opacity = "1";
-  document.querySelectorAll(".pageContent")[2].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[3].style.opacity = "0";
+  pageContent[0].style.opacity = "0";
+  pageContent[1].style.opacity = "1";
+  pageContent[2].style.opacity = "0";
+  pageContent[3].style.opacity = "0";
+  config.style.top = "0vh";
+  config.style.right = "0vw";
+  config.style.bottom = "5vh";
+  config.style.left = "5vw";
 }
 
 function toProjectsPage() {
-  document.querySelectorAll(".pageContent")[0].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[1].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[2].style.opacity = "1";
-  document.querySelectorAll(".pageContent")[3].style.opacity = "0";
+  pageContent[0].style.opacity = "0";
+  pageContent[1].style.opacity = "0";
+  pageContent[2].style.opacity = "1";
+  pageContent[3].style.opacity = "0";
+  config.style.top = "5vh";
+  config.style.right = "5vw";
+  config.style.bottom = "0vh";
+  config.style.left = "0vw";
 }
 
 function toContactPage() {
-  document.querySelectorAll(".pageContent")[0].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[1].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[2].style.opacity = "0";
-  document.querySelectorAll(".pageContent")[3].style.opacity = "1";
+  pageContent[0].style.opacity = "0";
+  pageContent[1].style.opacity = "0";
+  pageContent[2].style.opacity = "0";
+  pageContent[3].style.opacity = "1";
+  config.style.top = "5vh";
+  config.style.right = "0vw";
+  config.style.bottom = "0vh";
+  config.style.left = "5vw";
 }
 
 //https://stackoverflow.com/questions/58736328/horizontally-scrolling-by-a-full-page-on-key-press
