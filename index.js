63a3c31c81b237d2fa1c2724fe6af486dd9cbb88
iSamekh <ee-gomes@outlook.com>
2022-12-04 12:35:56 -0300

    Theme change

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-//https://stackoverflow.com/questions/6338217/get-a-css-value-with-javascript
+function themeChange() {
+  let elementBody = document.body;
+  elementBody.classList.toggle("dark-mode");
+  console.log("click");
+}
+
 
 let inConfig = false;
 
@@ -9,7 +14,8 @@ function configMenu() {
     inConfig = false;
   }
   else {
-    document.querySelector("#configMenu").style.width = "30vmin";
+    document.querySelector("#configMenu").style.width = "40vmin";
+
     document.querySelector("#configMenu").style.boxShadow = "0 0.4vmin 0.8vmin 0 rgba(0, 0, 0, 1), 0 0 2vmin 1vmin rgba(0, 0, 0, 0.7);"
 
     inConfig = true;
@@ -17,60 +23,52 @@ function configMenu() {
 }
 
 const pageContent = document.querySelectorAll(".pageContent");
+let actualPage = 0;
+
+
 const config = document.querySelector("#config");
 const configM = document.querySelector("#configMenu");
 
+function goUp() {
+  pageContent[actualPage].style.opacity = "0";
+  actualPage -= 2;
+  pageContent[actualPage].style.opacity = "1";
 
-function toAboutMe() {
-  pageContent[0].style.opacity = "1";
-  pageContent[1].style.opacity = "0";
-  pageContent[2].style.opacity = "0";
-  pageContent[3].style.opacity = "0";
-
-  config.style.right = "3vmin";
-  config.style.bottom = "3vmin";
-
-  configM.style.right = "3vmin";
-  configM.style.bottom = "3vmin";
+  config.style.bottom = "3vh";
+  configM.style.bottom = "3vh";
 }
 
-function toKnowledgePage() {
-  pageContent[0].style.opacity = "0";
-  pageContent[1].style.opacity = "1";
-  pageContent[2].style.opacity = "0";
-  pageContent[3].style.opacity = "0";
+function goRight() {
+  pageContent[actualPage].style.opacity = "0";
+  actualPage += 1;
+  pageContent[actualPage].style.opacity = "1";
 
-  config.style.right = "-11vmin";
-  config.style.bottom = "3vmin";
+  config.style.right = "";
+  config.style.left = "103vw";
 
-  configM.style.right = "-11vmin";
-  configM.style.bottom = "3vmin";
+  configM.style.right = "";
+  configM.style.left = "103vw";
 }
 
-function toProjectsPage() {
-  pageContent[0].style.opacity = "0";
-  pageContent[1].style.opacity = "0";
-  pageContent[2].style.opacity = "1";
-  pageContent[3].style.opacity = "0";
+function goDown() {
+  pageContent[actualPage].style.opacity = "0";
+  actualPage += 2;
+  pageContent[actualPage].style.opacity = "1";
 
-  config.style.right = "3vmin";
-  config.style.bottom = "-11vmin";
-
-  configM.style.right = "3vmin";
-  configM.style.bottom = "-11vmin";
+  config.style.bottom = "-11vh";
+  configM.style.bottom = "-11vh";
 }
 
-function toContactPage() {
-  pageContent[0].style.opacity = "0";
-  pageContent[1].style.opacity = "0";
-  pageContent[2].style.opacity = "0";
-  pageContent[3].style.opacity = "1";
+function goLeft() {
+  pageContent[actualPage].style.opacity = "0";
+  actualPage -= 1;
+  pageContent[actualPage].style.opacity = "1";
 
-  config.style.right = "-11vmin";
-  config.style.bottom = "-11vmin";
+  config.style.right = "3vw";
+  config.style.left = "";
 
-  configM.style.right = "-11vmin";
-  configM.style.bottom = "-11vmin";
+  configM.style.right = "3vw";
+  configM.style.left = "";
 }
 
 //https://stackoverflow.com/questions/58736328/horizontally-scrolling-by-a-full-page-on-key-press
