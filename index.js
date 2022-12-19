a9756491924d930de57163a1cbc087e064b7080d
Eric Gomes <31543471+iSamekh@users.noreply.github.com>
2022-12-19 10:40:18 -0300

    Page change refactory

--- index.js
+++ index.js
@@ -29,12 +29,30 @@ let actualPage = 0;
 const config = document.querySelector("#config");
 const configM = document.querySelector("#configMenu");
 
+
+function pageMove(position) {
+  pageContent[actualPage].style.opacity = "0";
+  actualPage += position;
+  pageContent[actualPage].style.opacity = "1";
+
+  /* config.style:
+   *  fixed at right and bottom, only change the values between positive and
+   *  negative for directions adjustments (transition won't work in other way)
+   */
+
+  /* configM.style:
+   * bottom shift between 3vh and -3vh from up to bottom
+   * shift between left and right (to properly set the menu slide direction)
+   */
+}
+
 function goUp() {
   pageContent[actualPage].style.opacity = "0";
   actualPage -= 2;
   pageContent[actualPage].style.opacity = "1";
 
   config.style.bottom = "3vh";
+
   configM.style.bottom = "3vh";
 }
 
@@ -56,6 +74,7 @@ function goDown() {
   pageContent[actualPage].style.opacity = "1";
 
   config.style.bottom = "-11vh";
+
   configM.style.bottom = "-11vh";
 }
 
