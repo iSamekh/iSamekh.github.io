61e8b8a57f0127b01c82fdcee63f9b81a6718f5e
Eric Gomes <31543471+iSamekh@users.noreply.github.com>
2022-12-19 13:31:08 -0300

    pageMove refactory

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 function themeChange() {
   let elementBody = document.body;
   elementBody.classList.toggle("dark-mode");
-  console.log("click");
 }
-
 
 let inConfig = false;
 
 function configMenu() {
   if (inConfig) {
     document.querySelector("#configMenu").style.width = "8vmin";
-
     inConfig = false;
   }
   else {
     document.querySelector("#configMenu").style.width = "40vmin";
-
     document.querySelector("#configMenu").style.boxShadow = "0 0.4vmin 0.8vmin 0 rgba(0, 0, 0, 1), 0 0 2vmin 1vmin rgba(0, 0, 0, 0.7);"
-
     inConfig = true;
   }
 }
 
-const pageContent = document.querySelectorAll(".pageContent");
 let actualPage = 0;
 
-
-const config = document.querySelector("#config");
-const configM = document.querySelector("#configMenu");
-
-
 function pageMove(position) {
+
+  const pageContent = document.querySelectorAll(".pageContent");
+  const config = document.querySelector("#config");
+  const configM = document.querySelector("#configMenu");
+
   pageContent[actualPage].style.opacity = "0";
   actualPage += position;
   pageContent[actualPage].style.opacity = "1";
@@ -39,45 +33,45 @@ function pageMove(position) {
     case 0:
       console.log("page 0 test")
 
-      config.style.right = "3vw";
-      config.style.bottom = "3vh";
+      config.style.right = "3vmin";
+      config.style.bottom = "3vmin";
 
       configM.style.left = "";
-      configM.style.right = "3vw";
-      configM.style.bottom = "3vh";
+      configM.style.right = "3vmin";
+      configM.style.bottom = "3vmin";
       break;
 
     case 1:
       console.log("page 1 test")
 
-      config.style.right = "-8vw";
-      config.style.bottom = "3vh";
+      config.style.right = "-8vmin";
+      config.style.bottom = "3vmin";
 
       configM.style.right = "";
-      configM.style.left = "103vw";
-      configM.style.bottom = "3vh";
+      configM.style.left = "103vmin";
+      configM.style.bottom = "3vmin";
       break;
 
     case 2:
       console.log("page 2 test")
 
-      config.style.right = "3vw";
-      config.style.bottom = "-11vh";
+      config.style.right = "3vmin";
+      config.style.bottom = "-8vmin";
 
       configM.style.left = "";
-      configM.style.right = "3vw";
-      configM.style.bottom = "-11vh";
+      configM.style.right = "3vmin";
+      configM.style.bottom = "-8vmin";
       break;
 
     case 3:
       console.log("page 3 test")
 
-      config.style.right = "-8vw";
-      config.style.bottom = "-11vh";
+      config.style.right = "-8vmin";
+      config.style.bottom = "-8vmin";
 
       configM.style.right = "";
-      configM.style.left = "103vw";
-      configM.style.bottom = "-11vh";
+      configM.style.left = "103vmin";
+      configM.style.bottom = "-8vmin";
       break;
   }
 
@@ -90,50 +84,6 @@ function pageMove(position) {
    * bottom shift between 3vh and -3vh from up to bottom
    * shift between left and right (to properly set the menu slide direction)
    */
-}
-
-function goUp() {
-  pageContent[actualPage].style.opacity = "0";
-  actualPage -= 2;
-  pageContent[actualPage].style.opacity = "1";
-
-  config.style.bottom = "3vh";
-
-  configM.style.bottom = "3vh";
-}
-
-function goRight() {
-  pageContent[actualPage].style.opacity = "0";
-  actualPage += 1;
-  pageContent[actualPage].style.opacity = "1";
-
-  config.style.right = "";
-  config.style.left = "103vw";
-
-  configM.style.right = "";
-  configM.style.left = "103vw";
-}
-
-function goDown() {
-  pageContent[actualPage].style.opacity = "0";
-  actualPage += 2;
-  pageContent[actualPage].style.opacity = "1";
-
-  config.style.bottom = "-11vh";
-
-  configM.style.bottom = "-11vh";
-}
-
-function goLeft() {
-  pageContent[actualPage].style.opacity = "0";
-  actualPage -= 1;
-  pageContent[actualPage].style.opacity = "1";
-
-  config.style.right = "3vw";
-  config.style.left = "";
-
-  configM.style.right = "3vw";
-  configM.style.left = "";
 }
 
 //https://stackoverflow.com/questions/58736328/horizontally-scrolling-by-a-full-page-on-key-press
