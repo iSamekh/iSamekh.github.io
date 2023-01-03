c3baa082b68afbb21213a63d176e9ad9b878f0ea
iSamekh <ee-gomes@outlook.com>
2023-01-03 17:50:34 -0300

    Glow Effect

--- index.js
+++ index.js
@@ -15,21 +15,22 @@ function menuSlider() {
   else {
     bottomMenu.style.animationName = "bottomMenuUp";
     bottomMenu.style.bottom = "1vh";
-    //document.querySelector("#configMenu").style.boxShadow = "0 0.4vmin 0.8vmin 0 rgba(0, 0, 0, 1), 0 0 2vmin 1vmin rgba(0, 0, 0, 0.7);"
     inMenu = true;
   }
 }
 
 let actualPage = 0;
 
+
 function pageMove(position) {
 
   const pageContent = document.querySelectorAll(".pageContent");
-  const config = document.querySelector("#config");
-  const configM = document.querySelector("#configMenu");
+  const nav = document.querySelectorAll("a");
 
+  nav[actualPage].classList.remove("glowText");
   pageContent[actualPage].style.opacity = "0";
   actualPage += position;
+  nav[actualPage].classList.add("glowText");
   pageContent[actualPage].style.opacity = "1";
 }
 
